721f6c4f3d2ed172a343783f6a40edea2df7af36
Simon Horsgaard <simonkrh@gmail.com>
2024-03-14 16:11:52 +0100

    Changed the course page and some small changes in the css's for the footer and navbar.

--- frontend/pages/course/image-scroll.js
+++ frontend/pages/course/image-scroll.js
@@ -1,21 +1,68 @@
-document.addEventListener('scroll', function() {
+function adjustSidebar() {
     var sidebar = document.querySelector('.sidebar');
 
-    if (sidebar.style.marginTop === "") {
-        sidebar.style.marginTop = '150px';
+    // Return if the sidebar doesn't exist
+    if (!sidebar) return;
+
+    if (window.innerWidth >= 1075) {
+        document.addEventListener('scroll', handleScroll);
+        handleScroll();
+    } else {
+        document.removeEventListener('scroll', handleScroll);
+        // Reset sidebar style for smaller screens
+        sidebar.style.position = '';
+        sidebar.style.top = '';
     }
+}
 
+function handleScroll() {
+    var sidebar = document.querySelector('.sidebar');
     var footer = document.querySelector('.footer');
-    var footerRect = footer.getBoundingClientRect();
-    var sidebarBottom = sidebar.getBoundingClientRect().bottom;
-    var footerOffsetTop = footer.offsetTop;
-    var stopFixed = footerOffsetTop - sidebar.offsetHeight - 170;
 
-    if (window.scrollY > stopFixed) {
+    if (!sidebar || !footer) return;
+
+    var footerOffsetTop = footer.offsetTop;
+    var sidebarHeight = sidebar.offsetHeight;
+    var stopFixed = footerOffsetTop - sidebarHeight - 400;
+
+    var initialTopOffset = 150;
+    var scrollTop = window.scrollY + initialTopOffset;
+
+    if (scrollTop > stopFixed) {
         sidebar.style.position = 'absolute';
         sidebar.style.top = stopFixed + 'px';
     } else {
         sidebar.style.position = 'fixed';
-        sidebar.style.top = '0px';
+        sidebar.style.top = initialTopOffset + 'px';
+    }
+}
+
+// For showing more course providers
+document.getElementById("showMoreButton").addEventListener("click", function() {
+    const hiddenProviders = document.querySelectorAll('.provider:nth-of-type(n+4)');
+    let showMore = true;
+
+    // Toggles the display property of hidden providers
+    hiddenProviders.forEach(function(provider) {
+        if (provider.style.display === 'none' || provider.style.display === '') {
+            provider.style.display = 'block';
+            showMore = false;
+        } else {
+            provider.style.display = 'none';
+            showMore = true;
+        }
+    });
+
+    // Toggles the button icon direction
+    const icon = this.querySelector('i');
+    if (showMore) {
+        icon.className = 'fa fa-chevron-down';
+    } else {
+        icon.className = 'fa fa-chevron-up';
     }
 });
+
+
+adjustSidebar();
+// Listen for window resize events
+window.addEventListener('resize', adjustSidebar);
