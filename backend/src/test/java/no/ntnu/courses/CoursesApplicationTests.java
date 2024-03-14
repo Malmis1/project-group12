a4dda2c3328a936e1839585a32755a7afbba0e5e
Andreas Malmedal <andreas07.malmedal@gmail.com>
2024-03-14 10:05:38 +0100

    Created a spring application.

---
+++ backend/src/test/java/no/ntnu/courses/CoursesApplicationTests.java
@@ -0,0 +1,11 @@
+package no.ntnu.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
