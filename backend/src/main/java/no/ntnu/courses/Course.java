c0eaba099dfb435aca4859f81a5e15b85edf4150
Andreas Malmedal <andreas07.malmedal@gmail.com>
2024-03-21 09:38:27 +0100

    Changed imports of entity classes. Formatted code.

--- backend/src/main/java/no/ntnu/courses/Course.java
+++ backend/src/main/java/no/ntnu/courses/Course.java
@@ -1,6 +1,12 @@
 package no.ntnu.courses;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Course {
@@ -40,7 +46,6 @@ public class Course {
     @ManyToOne
     @JoinColumn(name = "sessionId", nullable = false)
     private Session sessionId;
-
 
     public Category getCategoryId() {
         return this.categoryId;
--- backend/src/main/java/no/ntnu/courses/User.java
+++ backend/src/main/java/no/ntnu/courses/User.java
@@ -1,7 +1,10 @@
 package no.ntnu.courses;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
@@ -61,4 +64,3 @@ public class User {
         this.userId = userId;
     }
 }
-
